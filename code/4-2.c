dc60227e0ebfa94ed07806d020381d2596266910
H1yeee19 <happylacu@gmail.com>
2026-03-17 01:40:39 +0900

    Update 4-2.c

--- code/4-2.c
+++ code/4-2.c
@@ -4,7 +4,7 @@ int main()
 {
     for(int i=1; i<10; i++) {
         for (int n=1; n<10; n++) {
-            printf("%d * %d = %d\n",i,n,i*n");
+            printf("%d * %d = %d\n",i,n,i*n);
         }
     }
     return 0;
