9cc802aa08c812140753e1464fc94ebc6a77f6ea
H1yeee19 <happylacu@gmail.com>
2026-03-17 01:45:45 +0900

    Update printf statements for variable outputs

--- code/1-1.c
+++ code/1-1.c
@@ -4,18 +4,18 @@ int main()
 {
     int var1 = 100;
     printf("int : %d\n", var1);
-    printf("hex : %x\n", var1);
-    printf("oct : %o\n", var1);
+    printf("hex : %x\n", var1);    //16진법
+    printf("oct : %o\n", var1);    //8진법
 
     char var2 = 'c';
     printf("char : %c\n", var2);
 
-    char* var3 = "hello world!";
+    char* var3 = "hello world!";    //c언어에는 string 타입 존재X, char* 형식으로 표시
     printf("string : %s\n", var3);
 
     float var4 = 3.141592;
     printf("float : %f\n", var4);
-    printf("exp : %p\n", var4);
+    printf("exp : %e\n", var4);    //과학표기법
 
     return 0;
 }
