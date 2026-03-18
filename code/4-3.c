5265eee64a893bcb6d20563e56b48fb08e152909
H1yeee19 <happylacu@gmail.com>
2026-03-19 02:02:46 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -2,7 +2,17 @@
 
 int main()
 {
-
+    int a
+    scanf("%d", &a);
+    if ( a == 1 || a ==2 )
+        printf("true");
+    if ( a >2){
+        for (int i=2; i<a; i++) {
+            if (a%i == 0)
+                printf("false");
+            else
+                printf("true");
+        }
     return 0;
 }
 
